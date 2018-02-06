f40230bd4592599a9c0df046742217cb35e4e79b
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-02-05 20:48:21 -0500

    everything working except position of popup form

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ $(document).ready(function(){
 		$(this).addClass("reserved");
 		$(this).removeClass("available");
 	});
+
+	$(".circles").click(function() {
+		$("#hiddenForm").removeClass("hidden");
+	});
+
+	$(".circles").click(function() {
+		var digit = $(this).text();
+		$("#tableDigit").text(digit);
+	})
 	
 });
