225993cd9485180ca3d446b5ebf672610cd298fb
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-02-06 14:56:36 -0500

    Lab 1 requirements met

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ $(document).ready(function(){
 	var table;
 
 	$(".tables").click(function() {
-		table = $(this)
+		table = $(this);
 	});
+
+	$(".tables").hover(function() {
+		tableHover = $(this);
+	})
 
 	$(".tables").click(function() {
 		if (table.hasClass("available")) {
@@ -20,11 +24,28 @@ $(document).ready(function(){
 	$("#saveButton").click(function() {
 		$(table).addClass("reserved");
 		$(table).removeClass("available");
+		$(table).addClass("notAllowed");
 		$("#hiddenForm").addClass("hidden");
 	});
 
 	$("#quitButton").click(function() {
 		$("#hiddenForm").addClass("hidden");
+	});
+
+	$(".tables").hover(function() {
+		if (tableHover.hasClass("available")) {
+			$(tableHover).css("border", "2px dashed #E34E45");
+		}
+	}, function() {
+		$(tableHover).css("border", "none");
+	});
+
+	$(".tables").hover(function() {
+		if (tableHover.hasClass("available")) {
+			$(tableHover).css("cursor", "crosshair");
+		}
+	}, function() {
+		$(tableHover).css("cursor", "");
 	});
 	
 });
